0677caeaf5ce92da4978b280685b0d27a5e90b52
MacBook Pro <macbookpro@MacBookui-MacBookPro.local>
2020-10-23 11:49:01 +0900

    8- map : 많은 div들을 반복문으로 줄이고 싶은 충동이 들 때

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,30 +5,27 @@ import './App.css';
 function App() {
 
   let [글제목,글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '강남 고기 맛집']); 
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
 
   let [modal, modal변경] = useState(false);
 
-  /*
-  function 제목바꾸기(){
-    var newArray = [...글제목]; 
-    newArray[0] = '여자 코트 추천';
-    글제목변경( newArray );
-  }
-  */
+  var 어레이 = [2,3,4];
+  {/* map을 사용하면 각각의 값에 *2를 해서 새로운 어레이가 생성 된다. => 유사 반복문*/}
+  var 뉴어레이 = 어레이.map(function(a){
+    return a*2
+  });
   
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
+      {/*
       <div className="list">
         <h3> {글제목[0]} <span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
-
-      {/*<button onClick={ 제목바꾸기 }>버튼</button>*/}
 
       <div className="list">
         <h3> {글제목[1]}</h3>
@@ -41,18 +38,28 @@ function App() {
         <p>2월 10일 발행</p>
         <hr/>
       </div>
+      */}
+    {
+      글제목.map(function(글, 좋아요){ //파라미터가 차례대로 넘어온다.
+        return ( 
+        <div className="list">
+              <h3> {글} <span onClick={ ()=>{
+                var newArray = [...따봉];
+                newArray[좋아요]++;
+                따봉변경(newArray) 
+                } }>👍</span> {따봉[좋아요]}</h3>
+              <p>2월 16일 발행</p>
+              <hr/>
+          </div>
+        )  
+      })
+    }
 
       <button onClick={ ()=> { modal변경(!modal)}}>버튼</button>
-      {/* 리액트에서 if문은 동작하지 않는다. 대신 삼항연산자를 사용한다! */}
       {
         modal === true
         ? <Modal></Modal>
-        : null /* 텅 빈 HTML */
-        /*
-        1 < 3 
-        ? console.log('맞아요')
-        : console.log('틀려요')
-        */
+        : null 
       }
     </div>
   );
