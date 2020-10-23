7b3f27b8e32b6da967762763679df933da5f3f3e
MacBook Pro <macbookpro@MacBookui-MacBookPro.local>
2020-10-23 10:37:17 +0900

    6- Component : 많은 div들을 한 단어로 줄이고 싶은 충동이 들 때

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,20 +8,9 @@ function App() {
   let [따봉, 따봉변경] = useState(0);
 
   function 제목바꾸기(){
-    {/* 
-      미리 정의된 함수를 집어 넣을 때는 소괄호를 넣지 않는게 포인트
-      ()를 적게되면 바로 실행되기 때문에. 버튼을 눌렀을 때 실행이 되어야 함!
-    */}
-    {/*var newArray = 글제목;  이렇게 복사하면 reference copy가 된다 주의 */}
-    var newArray = [...글제목]; {/* deep copy!!! ...기호는 중괄호나 대괄호를 벗겨달라는 뜻으로 사용되거나 array/object자료형을 deepcopy시 사용 */}
+    var newArray = [...글제목]; 
     newArray[0] = '여자 코트 추천';
     글제목변경( newArray );
-    {/* 
-      state 변경 방법 (꼭 외우세요!!!)
-      1. 일단 기존 state 카피본을 만들고
-      2. 카피본에 수정사항 반영하고
-      3. 변경함수()에 집어넣기
-    */}
   }
   
   return (
@@ -48,8 +37,39 @@ function App() {
         <p>2월 10일 발행</p>
         <hr/>
       </div>
+
+      <Modal />
+      {/* 
+        Component 만들때 주의 사항
+        1. 이름은 대문자로 시작
+        2. return 소괄호 안에 내용을 담는데, return()안에 있는 건 태그 하나로 묶어야함
+
+        Component를 사용하면,
+        컴포넌트 안에 컴포넌트를 넣는 등 관리가 편해짐
+
+        어떤 것들을 Component로 만드는 것이 좋을까?
+        - 반복 출현하는 HTML 덩어리들
+        - 자주 변경되는 HTML UI들
+        - 다른 페이지를 만들 때
+
+        단점
+        - State를 사용할 때 복잡해짐
+          -> 상위 컴포넌트에서 만든 state를 사용하려면 props문법을 이용해야함
+       */}
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <> {/* div가 쓰기 싫다면 <>, </> 로 해주어도 됨 */}
+    <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
