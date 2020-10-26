8487c650fdb709b4605bad537ebd0e65c820432f
MacBook Pro <macbookpro@MacBookui-MacBookPro.local>
2020-10-26 13:03:22 +0900

    ShoppingMall 1- 쇼핑몰 프로젝트 : 프로젝트 생성 & Bootstrap 설치

---
+++ shop/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* HTML/CSS 디자인 쌩코딩이 싫다면 Bootstrap 이용하기
+        getbootstrap.com -> react 전용 bootstrap 
+      */}
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
