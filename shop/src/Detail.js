c1a1a7f448f0b08c160734875141ba56845f8b75
MacBook Pro <macbookpro@MacBookui-MacBookPro.local>
2020-10-31 14:45:30 +0900

    10- Lifecycle Hook (옛날사람) useEffect (요즘사람)

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useHistory, useParams} from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss'
@@ -12,8 +12,33 @@ let 제목 = styled.h4`
     color : ${props => props.색상}
 `;
 
+// //Hook이라는 것을 사용해서 컴포넌트가 일할 때 중간 중간에 참견을 할 수 있다.
+// //ex. Detail 컴포넌트 등장 전에 이것 좀 해줘
+// class Detail2 extends React.Component{
+//   componentDidMount(){
+//     //Detail2 컴포넌트가 Mount 되고나서 실행할 코드
+//     //컴포넌트 첫 등장 후 실행할 코드
+//   }
+//   componentWillUnmount(){
+//     //Detail2 컴포넌트가 UnMount 되고나서 실행할 코드
+//     //다른페이지로 넘어간다든지 등의 사유로 컴포넌트가 사라지기 전 실행 할 코드
+//   }
+// }
 
 function Detail(props){
+
+  useEffect(()=>{  // hook은 여러개 사용하고 싶으면 여러개도 생성 가능하다. (대신 코드 적힌 순서대로 실행)
+    //컴포넌트가 mount, update 되었을 때 실행 가능
+    //2초 후에 alert 창을 보이지 않게 하려면
+    let 타이머 = setTimeout(()=>{
+      
+    }, 2000);
+    return function 어쩌구() {
+      //실행할 코드 
+      // unmount 될 때 실행. (컴포넌트 사라질 때 실행되는 부분)
+    }
+  });
+
     let { id } = useParams(); 
     let history = useHistory();
 
