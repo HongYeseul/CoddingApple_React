e290a8170262ca821090b58dd2047a23042a4ca2
MacBook Pro <macbookpro@MacBookui-MacBookPro.local>
2020-12-30 13:47:21 +0900

    리액트에서의 Ajax 요청방법 & Ajax는 무엇인가

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,8 +3,19 @@ import { Navbar, Nav, NavDropdown, Button, Jumbotron } from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
 import Detail from './Detail.js';
+import axios from 'axios';
 
 import {Link, Route, Switch} from 'react-router-dom'
+
+// Ajax : 서버에 새로고침 없이 요청을 할 수 있게 도와주는 기술
+// 서버에 요청을 할 수 있는 방법은 여러종류가 있는데,
+// GET 요청 : 주소창에 URL로 표현 -> 특정 페이지 / 자료 읽기에 사용
+// POST 요청 : 서버로 중요 정보 전달 시 사용
+
+// Ajax 사용 방법
+// 1. jQuery 설치해서 $.ajax() 사용
+// 2. axios 설치해서 axios.get() 사용
+// 3. JS - fetch() 사용 : 호환성 문제로 잘 쓰이지 않음
 
 function App() {
 
@@ -52,6 +63,22 @@ function App() {
               })
             }
           </div>
+          <button className="btn btn-primary" onClick={()=>{
+
+            // 버튼을 누르게되면
+            // 1. axios.get(데이터 요청할 URL)
+            // 2. 성공하면.then()
+            // 3. 실패하면 .catch()
+
+            axios.get('https://codingapple1.github.io/shop/data2.json')
+            .then((result)=>{
+              console.log("Complete");
+              console.log(result.data); // 그냥 result 값에는 통신 정보 등의 모든 정보가 들어있게 됨.
+            })
+            .catch(()=>{
+              console.log("FAIL");
+            })
+          }}>더보기</button>
         </div>
       </Route>
 
